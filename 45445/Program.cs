99e1da7efec3d4cb83d13c636acafecd8722f310
Товкус Артем Евгеньевич <PIBO0121020@sibupk.su>
2023-09-30 20:47:45 +0700

    ffk,

--- 45445/Program.cs
+++ 45445/Program.cs
@@ -10,15 +10,41 @@ namespace _45445
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(M2());
+            //Console.WriteLine(C1(10));
+            var param1 = int.Parse(Console.ReadLine());
+            double res4 = Proc3_Amean(param1, 30);
+            Console.WriteLine($"Среднее арифметическое: {res4}");
+            var param2 = int.Parse(Console.ReadLine());
+            double res5 = Proc4_Gmean(param1, 30);
+            Console.WriteLine($"Среднее геометрическое: {res5}");
+            Console.ReadKey();
+
         }
 
-        static string M1()
-        {
-            Console.WriteLine("Введите количество рублей: ");
-            string tmp = Console.ReadLine();
-            int a = int.Parse(tmp);
 
+        //static тип результат или void ИмяФункции( список параметров вида тип_переменной ИмяПеременной, ...)
+        //{
+        //  тело метода
+        // return значение, если тип результата не void
+        //}
+
+        static double Proc3_Amean(double X, double Y)
+        {
+            double amean = (X + Y) / 2;
+            return amean;
+        }
+        static double Proc4_Gmean(double X, double Y)
+        {
+            double gmean = Math.Sqrt(X * Y);
+            return gmean;
+        }
+
+
+
+
+
+        static string M1(int a)
+        {
             if (a < 0)
             {
                 return "Денежная валюта не может быть отрицательной (пока что)";
@@ -40,11 +66,8 @@ namespace _45445
             }
         }
     
-        static string M2()
+        static string M2(int a)
         {
-            Console.WriteLine("Введите трехзначное число");
-            string tmp = Console.ReadLine();
-            int a = int.Parse(tmp);
 
             if (100 > a || a > 999)
             {
@@ -65,8 +88,8 @@ namespace _45445
                 }
             }
         }
+        
 
-      
 
     }
 }
